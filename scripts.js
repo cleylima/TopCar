98098717e5520a2acaea8296127567c023a5c922
Cleyson Lima <162802527+cleylima@users.noreply.github.com>
2025-02-22 11:07:03 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,40 +1,35 @@
-let prevButton = document.getElementById('prev')
-let nextButton = document.getElementById('next')
-let container = document.querySelector('.container')
-let items = document.querySelectorAll('.list .item')
-let indicator = document.querySelector('.indicators')
-let dots = indicator.querySelectorAll('ul li')
-let list = container.querySelector('.list')
+let prevButton = document.getElementById('prev');
+let nextButton = document.getElementById('next');
+let container = document.querySelector('.container');
+let items = document.querySelectorAll('.list .item');
+let indicator = document.querySelector('.indicators');
+let dots = indicator.querySelectorAll('ul li');
+let list = container.querySelector('.list');
 
-
-let active = 0
-let lastPosition = items.length - 1
+let active = 0;
+let lastPosition = items.length - 1;
 
 function setSlider() {
-    let itemOld = container.querySelector('.list .item.active')
-    itemOld.classList.remove('active')
+    let itemOld = container.querySelector('.list .item.active');
+    itemOld.classList.remove('active');
 
-    let dotsOld = indicator.querySelector('ul li.active')
-    dotsOld.classList.remove('active')
-    dots[active].classList.add('active')
+    let dotsOld = indicator.querySelector('ul li.active');
+    dotsOld.classList.remove('active');
+    dots[active].classList.add('active');
 
-    indicator.querySelector('.number').innerHTML = '0' + (active + 1)
+    indicator.querySelector('.number').innerHTML = '0' + (active + 1);
 }
-
 
 nextButton.onclick = () => {
-    list.style.setProperty('--calculation', 1)
-    
-    active = active + 1 > lastPosition ? 0 : active + 1
-    setSlider()
-    items[active].classList.add('active')
-}
-
+    items[active].classList.remove('active');
+    active = (active + 1 > lastPosition) ? 0 : active + 1;
+    items[active].classList.add('active');
+    setSlider();
+};
 
 prevButton.onclick = () => {
-    list.style.setProperty('--calculation', -1)
-
-    active = active - 1 < 0 ? lastPosition : active - 1
-    setSlider()
-    items[active].classList.add('active')
-}
+    items[active].classList.remove('active');
+    active = (active - 1 < 0) ? lastPosition : active - 1;
+    items[active].classList.add('active');
+    setSlider();
+};
